3230689d446b6f30382ece881993925170ef5d55
Bao Trinh <tqbao1717@gmail.com>
2024-09-18 19:58:11 -0400

    hi

---
+++ CPAN113/lab1.js
@@ -0,0 +1,10 @@
+var userName = 'Bao';
+let userAge = 17
+const userID = 01707
+
+let score = 95.5;
+let courseName = "Fundamental JavaScript";
+let isEnrolled = true;
+let grades = [90, 95, 88, 92];
+let student = {name: "Your Name", age: Your Age, course: "CPAN"};
+
